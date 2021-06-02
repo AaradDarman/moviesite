f15ed4bcb8d8a7bf052b9c5765db97b502c34b2a
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-06-02 17:35:48 +0430

    Create Index Page and Add It To MovieSite

--- src/containers/MovieSite.jsx
+++ src/containers/MovieSite.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Carousel from "../components/Carousel";
 import MainLayout from "../components/layouts/MainLayout";
+import Index from "../pages/Index";
 
 const MovieSite = (props) => {
-  return <MainLayout {...props}></MainLayout>;
+  return (
+    <MainLayout {...props}>
+      <Switch>
+        <Route path="/" component={Index} />
+      </Switch>
+    </MainLayout>
+  );
 };
 
 export default MovieSite;
---
+++ src/pages/Index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import MainLayout from "../components/layouts/MainLayout";
+
+const film = {
+  specialOffers: ["همراه با نسخه دوبله فارسی", "همراه با زیرنویس چسبیده"],
+  title: "The Courier",
+  imgUrl: "/images/TheCourier.jpg",
+  imdbRate: "7.1",
+  votersNumber: "15000",
+  resolution: "BluRay x256 10bit 1080p",
+  genres: ["هیجان انگیز"],
+  year: "2020",
+  country: "انگلستان",
+  director: "Dominic Cooke",
+  actors: [
+    "Fred Haig ",
+    "James Schofield",
+    "Merab Ninidze",
+    "Vladimir Chuprikov",
+  ],
+  ageRate: "pg-13",
+  summary:
+    "گریویل وین، جاسوس جنگ سرد و منبع روسی اش سعی می کنند بحران موشکی کوبا را خاتمه دهند...",
+  createAt: "11 خرداد 1400",
+  comments: "14",
+};
+const Index = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Index;
