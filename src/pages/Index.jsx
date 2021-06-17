3fad4de780e9aa872db6e4a636ae36f67e99d92b
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-06-17 11:15:42 +0430

    Make WeeklySchedule Responsive to Path in Index Page And Fix Pagination

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,12 +5,18 @@ import WeeklySchedule from "../components/WeeklySchedule";
 
 const Index = (props) => {
   const { movies } = useSelector((state) => state);
+  const { match, history } = props;
+
+  const handlePageChange = (selected) => {
+    history.push(`/page/${selected + 1}`);
+  };
   return (
     <>
-      <WeeklySchedule />
+      {match.params.pageNumber == 1 || match.path === '/' ? <WeeklySchedule /> : null}
       <Movies
         items={movies}
         totalItems={100}
+        onPageChange={handlePageChange}
         {...props}
       />
     </>
