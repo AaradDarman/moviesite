14bd9f1047dbde10c24510ae03a0803a705660f1
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-06-17 10:43:21 +0430

    Create Download Route And Its Component

--- src/containers/MovieSite.jsx
+++ src/containers/MovieSite.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Carousel from "../components/Carousel";
 import MainLayout from "../components/layouts/MainLayout";
+import DownloadPage from "../pages/DownloadPage";
 import Index from "../pages/Index";
 
 const MovieSite = (props) => {
   return (
-    <MainLayout {...props}>
-      <Switch>
-        <Route path="/" component={Index} />
-      </Switch>
-    </MainLayout>
+    <Switch>
+      <MainLayout {...props}>
+        <Route path={["/"]}>
+          <Switch>
+            <Route
+              path={["/movie/download/:title", "/tv-show/download/:title"]}
+              component={DownloadPage}
+            />
+            <Route path="/" exact component={Index} />
+            <Route path="*" exact render={() => <div>Not Found</div>} />
+          </Switch>
+        </Route>
+      </MainLayout>
+    </Switch>
   );
 };
 
---
+++ src/pages/DownloadPage.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Movie from "../components/Movie";
+import { setMovie } from "../redux/actions/movie";
+
+const DownloadPage = ({ match }) => {
+  const dispatch = useDispatch();
+  const { movies, tvShows, movie } = useSelector((state) => state);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    console.log(match);
+    if (match.path.search("/movie") != -1) {
+      dispatch(
+        setMovie(
+          movies.find((movie) => {
+            return movie.title === match.params.title;
+          })
+        )
+      );
+    } else if (match.path.search("/tv-show") != -1) {
+      console.log("else");
+      dispatch(
+        setMovie(
+          tvShows.find((tvShow) => {
+            return tvShow.title === match.params.title;
+          })
+        )
+      );
+    }
+  }, []);
+  return (
+    <>
+      <Movie film={movie} />
+    </>
+  );
+};
+
+export default DownloadPage;
