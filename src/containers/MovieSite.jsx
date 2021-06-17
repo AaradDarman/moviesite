d173d1c1fe33f1e90488901fd31a04573da38236
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-06-17 11:21:38 +0430

    Add Routes To MovieSite.jsx

--- src/containers/MovieSite.jsx
+++ src/containers/MovieSite.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import MainLayout from "../components/layouts/MainLayout";
 import DownloadPage from "../pages/DownloadPage";
 import Index from "../pages/Index";
+import MoviesPage from "../pages/MoviesPage";
 
 const MovieSite = (props) => {
   return (
@@ -12,9 +13,24 @@ const MovieSite = (props) => {
         <Route path={["/"]}>
           <Switch>
             <Route
+              path={[
+                "/movie",
+                "/movie/page/:pageNumber",
+                "/movie/:category",
+                "/movie/:category/page/:pageNumber",
+                "/tv-show",
+                "/tv-show/page/:pageNumber",
+                "/tv-show/:category",
+                "/tv-show/:category/page/:pageNumber",
+              ]}
+              exact
+              component={MoviesPage}
+            />
+            <Route
               path={["/movie/download/:title", "/tv-show/download/:title"]}
               component={DownloadPage}
             />
+            <Route path="/page/:pageNumber" exact component={Index} />
             <Route path="/" exact component={Index} />
             <Route path="*" exact render={() => <div>Not Found</div>} />
           </Switch>
