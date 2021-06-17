a98e86b774daab3e0e7b30d97344e40b467f2d81
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-06-17 10:57:12 +0430

    Add Download Section To Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -192,6 +192,173 @@ const SummaryWraper = styled.div`
   }
 `;
 
+const DownloadSection = styled.div`
+  font-size: 0.8rem;
+  .download-title {
+    display: flex;
+    align-items: center;
+    margin: 1rem 0;
+  }
+  .download-title > span {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.primary.main};
+    border-radius: 2rem;
+    padding: 10px;
+    margin-left: 5px;
+  }
+  .download-title span i {
+    width: 20px;
+    height: 20px;
+    color: #bfbfbf;
+    background-color: ${({ theme }) => theme.text};
+    -webkit-mask: url("/images/inbox.svg");
+    mask: url("/images/inbox.svg");
+    display: inline-block;
+    -webkit-mask-repeat: no-repeat;
+    mask-repeat: no-repeat;
+    -webkit-mask-position: center;
+    mask-position: center;
+    -webkit-mask-size: cover;
+    mask-size: cover;
+  }
+  .trailer-link {
+    display: flex;
+    align-items: center;
+    padding: 0.3rem 0.5rem;
+    border-radius: 0.3rem;
+    color: ${({ theme }) => LightenDarkenColor(theme.secondary.light, 20)};
+    background-color: ${({ theme }) => hexToRGBA(theme.secondary.main, 0.2)};
+  }
+  .trailer-link i {
+    font-size: 1.2rem;
+    margin-right: auto;
+  }
+`;
+const DownloadLinksWraper = styled.div`
+  font-size: 0.8rem;
+  .bp3-button {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: inherit;
+    width: 100%;
+    background-color: ${({ theme }) => theme.primary.main};
+    background-image: none;
+    color: ${({ theme }) => LightenDarkenColor(theme.text, 50)};
+    margin-bottom: 1rem;
+    padding: 0.5rem;
+    border-radius: 0.3rem;
+    text-align: right;
+    box-shadow: none;
+    font-size: inherit;
+    transition: all 300ms ease-in-out;
+  }
+  .bp3-button:hover {
+    background-color: ${({ theme }) => theme.primary.main};
+    box-shadow: none;
+  }
+  .bp3-button.open {
+    background-color: ${({ theme }) =>
+      LightenDarkenColor(theme.primary.main, 20)};
+  }
+  .bp3-button.open:hover {
+    background-color: ${({ theme }) =>
+      LightenDarkenColor(theme.primary.main, 20)};
+    box-shadow: none;
+  }
+  .bp3-button::after {
+    content: "مشاهده لینک ها";
+    background-color: ${({ theme }) => theme.primary.light};
+    padding: 0 4px;
+    border-radius: 0.3rem;
+  }
+  .bp3-menu {
+    background-color: inherit;
+    padding: 0;
+    color: inherit;
+    text-align: right;
+    font-size: inherit;
+  }
+  .bp3-menu li {
+    padding: 0.5rem 0;
+  }
+  .bp3-menu-item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: 1px solid ${({ theme }) => theme.text};
+    border-radius: 0.3rem;
+    padding: 1rem 0;
+    cursor: inherit;
+  }
+  .bp3-menu-item:hover {
+    background-color: inherit;
+  }
+  .bp3-menu-item a {
+    padding: 0.2rem 0.5rem;
+    margin-left: 0.5rem;
+    border-radius: 1rem;
+    color: inherit;
+    transition: all 300ms ease-in-out;
+  }
+  .bp3-menu-item a:hover {
+    background-color: ${({ theme }) => theme.accent} !important;
+    color: black;
+    border-color: ${({ theme }) => theme.primary.light} !important;
+  }
+  .bp3-menu-item a:first-of-type {
+    margin-right: auto;
+    background-color: ${({ theme }) => theme.primary.light};
+    border: 1px solid ${({ theme }) => theme.text};
+  }
+  .bp3-menu-item a:last-of-type {
+    background-color: ${({ theme }) => theme.primary.main};
+    border: 1px solid ${({ theme }) => theme.primary.main};
+  }
+  .bp3-menu-item span {
+    text-transform: uppercase;
+  }
+  .bp3-menu-item span:first-child {
+    background: linear-gradient(90deg, #c9171f, #900000);
+    color: ${({ theme }) => (theme.isDark ? theme.text : "#fff")};
+    padding: 0.2rem 0.5rem;
+    border-radius: 1rem;
+  }
+`;
+
+const DownloadLinks = ({ item }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button className={open ? "open" : null} onClick={() => setOpen(!open)}>
+        {item.title}
+      </Button>
+      <Collapse isOpen={open}>
+        <Menu>
+          {item.linkItems &&
+            item.linkItems.map((link) => (
+              <li>
+                <div className="bp3-menu-item rtl">
+                  <span>{`کیفیت ${link.resolution}`}</span>
+                  <span>{`فرمت: ${link.format}`}</span>
+                  {link.encoder ? (
+                    <span>{`انکودر:${link.encoder}`}</span>
+                  ) : null}
+                  <span>{`حجم: ${link.size}`}</span>
+                  <a href={link.subtitleLink}>زیرنویس</a>
+                  <a href={link.dlLink}>لینک دانلود</a>
+                </div>
+              </li>
+            ))}
+        </Menu>
+      </Collapse>
+    </>
+  );
+};
+
 const Footer = styled.footer`
   display: flex;
   flex-direction: row;
@@ -349,6 +516,28 @@ const Movie = ({ film }) => {
           {film.summary}
         </p>
       </SummaryWraper>
+      {match.params.title ? (
+        <DownloadSection>
+          {film.trailerLink ? (
+            <a href={film.trailerLink} className="trailer-link">
+              {film.type === "movie"
+                ? "دانلود تریلر فیلم"
+                : "دانلود تریلر سریال "}
+              <i class="fab fa-youtube"></i>
+            </a>
+          ) : null}
+          <span className="download-title">
+            <span>
+              <i></i>
+            </span>
+            لینک های دانلود
+          </span>
+          <DownloadLinksWraper>
+            {film.downloadLinks &&
+              film.downloadLinks.map((item) => <DownloadLinks item={item} />)}
+          </DownloadLinksWraper>
+        </DownloadSection>
+      ) : null}
       <Footer className="flex-column flex-lg-row">
         <ul className="m-lg-0 mb-2">
           <li>
